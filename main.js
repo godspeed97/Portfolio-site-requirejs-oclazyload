a6487bf355341810c5a82c51754d6415d88ac9b6
oliviertr <olivier.tr@hotmail.com>
2016-01-14 23:54:16 -0500

    Fixed nav menu

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 require.config({
 
     paths: {
+        'jquery': ['https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.0/jquery.min', 'node_modules/jquery/dist/jquery.min'],
+        'bootstrapjs': ['https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap'],
         'angular': ['https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.7/angular.min', 'node_modules/angular/angular.min'],
         'angular-animate': ['https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.7/angular-animate.min', 'node_modules/angular-animate/angular-animate.min'],
         'angular-ui-router': ['https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.15/angular-ui-router.min', 'node_modules/angular-ui-router/build/angular-ui-router.min'],
@@ -16,6 +18,9 @@ require.config({
             exports: 'angular'
         },
         //---//
+        'bootstrapjs': {
+            deps: ['jquery']
+        },
         'angular-animate': {
             deps: ['angular']
         },
@@ -32,7 +37,7 @@ require.config({
             deps: ['angular', 'controllersModule']
         },
         'app': {
-            deps: ['angular', 'angular-ui-router', 'angular-animate', 'oclazyload', 'controllersModule', 'contactFormController']
+            deps: ['jquery', 'bootstrapjs', 'angular', 'angular-animate', 'angular-ui-router', 'oclazyload', 'controllersModule', 'contactFormController']
         }
     }
 });
